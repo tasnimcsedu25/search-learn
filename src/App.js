3cbf17dd0c18361ab0f8971144306a3612eb2c82
Shadman Shahriar <shadman.shahriar@dsinnovators.com>
2023-05-14 13:08:54 +0600

    filtered package

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css"
+import Header from "./components/common/header/Header"
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import About from "./components/about/About"
+import CourseHome from "./components/allcourses/CourseHome"
+import Footer from "./components/common/footer/Footer"
+import Home from "./components/home/Home"
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route exact path='/about' element={<About/>} />
+          <Route exact path='/courses' element={<CourseHome/>} />
+          {/* <Route exact path='/team' element={Team} />
+          <Route exact path='/pricing' element={Pricing} />
+          <Route exact path='/journal' element={Blog} />
+          <Route exact path='/contact' element={Contact} /> */}
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import AboutCard from "../about/AboutCard"
+import HAbout from "./HAbout"
+import Hero from "./hero/Hero"
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <AboutCard />
+      <HAbout />
+      {/* <Testimonal /> */}
+      {/* <Hblog /> */}
+      {/* <Hprice /> */}
+    </>
+  )
+}
+
+export default Home
